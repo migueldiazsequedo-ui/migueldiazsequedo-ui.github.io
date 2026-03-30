2dddb807ad2bc9a7ada8a5e5b33641c28603f52b
migueldiazsequedo-ui <miguel.diazsequedo@gmail.com>
2026-03-30 16:33:10 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  openCageApiKey: "55d4ce230a0b40b78af90e35e9d8c010",
+  metalApiKey: "38b91d4c6f65bdc7a0d5d18de017f839",
+  currencyApiKey: "64089c5c79d6033b34ab6b2db56a0e02"
+};
